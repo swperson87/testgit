b2a578983aadee0f811a1857f3c3c34f3f2ba45c
swperson87 <swperson87@gmail.com>
2023-09-08 14:16:18 +0900

    변경된 내용추가

--- src/edu/GitTestMain.java
+++ src/edu/GitTestMain.java
@@ -5,6 +5,7 @@ public class GitTestMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		System.out.println("변경된 내용 추가");
 	}
 
 }
